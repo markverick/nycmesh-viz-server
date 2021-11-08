035a858a196096a81c9b97fd6621090a404baeca
Zizhen Song <zizhensong@gmail.com>
2021-11-08 13:45:47 -0800

    RouterOS api get neighbors

---
+++ api_wrapper/api_helper.py
@@ -0,0 +1,33 @@
+from RouterOS_API.routeros_api import Api
+
+with open('secrets.txt') as f:
+  USER = f.readline()
+  PASSWORD = f.readline()
+
+
+# inital demo code
+# unseen = {'10.69.40.26'}
+# seen = set()
+
+# while(len(unseen) > 0):
+#   cur = unseen.pop()
+#   router = Api(cur, user='', password='')
+#   routes = router.talk('/routing/ospf/neighbor/print')
+#   seen.add(cur)
+#   print(routes)
+#   # print(router.talk('/ip/route/print'))
+
+#   for route in routes:
+#     parts = route["address"].split('/')
+#     address = parts[0]
+#     print(cur + " -> " + route["address"])
+#     if address not in seen:
+#       unseen.add(address)
+
+def get_neighbors_verbose(ip):
+  router = Api(ip, user='', password='')
+  return router.talk('/routing/ospf/neighbor/print')
+
+def get_neighbors(ip):
+  routes = get_neighbors_verbose(ip)
+  return ({route["address"], route["interface"]} for route in routes)
---
+++ api_wrapper/test.py
@@ -0,0 +1,4 @@
+from api_helper import get_neighbors, get_neighbors_verbose
+
+print(get_neighbors('10.69.40.26'))
+print(get_neighbors_verbose('10.69.40.26'))
