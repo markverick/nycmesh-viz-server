0ede794c839959e8f5f272225a283fd4aba7582f
Mark <sirapop741@gmail.com>
2021-12-10 00:59:52 -0800

    Fixed wrong tiebreaker & Fixed duplicated fetch_edges

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get("/fetch_edges", async (req, res) => {
     node = mergeSxt(data.v);
     if (!node.startsWith('sxt')) {
       // Sanitize output
-      if (dict[data.v] == undefined) {
+      if (dict[data.v] == undefined && data.v != v) {
         result.push({ "nn": data.v, "cost": data.w });
         dict[data.v] = data.w
       }
@@ -223,7 +223,7 @@ function pathFinding(y, x, badNodes) {
       if (badNodes.includes(v.v)) {
         continue;
       }
-      if (u.w[0] + v.w < dist[v.v][0] && u.w[1] + 1 < dist[v.v][1]) {
+      if (u.w[0] + v.w < dist[v.v][0] ||  (u.w[0] + v.w == dist[v.v][0] && u.w[1] + 1 < dist[v.v][1])) {
         dist[v.v] = [u.w[0] + v.w, u.w[1] + 1];
         pq.add(new Vertex(v.v, dist[v.v]));
         prev[v.v] = [u.v, v.w];
